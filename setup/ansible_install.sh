e28ad014d3a37d942f32b938fb978154fff33edf
ldaijiw <lwaltmann@spartaglobal>
2020-12-10 15:51:39 +0000

    changes to app playbook

--- setup/ansible_install.sh
+++ setup/ansible_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo Ansible instance IP address:
+
+read ip_addr
+
+ssh -i ~/.ssh/eng74_leo_aws_key.pem ubuntu@$ip_addr << EOF
+
+
+
 sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
@@ -15,3 +23,9 @@ pip3 install --upgrade pip
 pip3 install awscli -y
 pip3 install boto -y
 pip3 install boto3 -y
+
+EOF
+
+scp -i ~/.ssh/eng74_leo_aws_key.pem -r playbooks/ ubuntu@$ip_addr:/playbooks/ 
+
+ssh -i ~/.ssh/eng74_leo_aws_key.pem ubuntu@$ip_addr 
